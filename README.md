618c06692fb05f0d55b3cc09b2aee86b8a772e47
Trevor Dilley <trevor@twentyideas.com>
2019-04-01 06:52:09 -0700

    Shield arc and vulnerability ui implemented, updated readme

--- README.md
+++ README.md
@@ -15,31 +15,28 @@ management twist!
 ## TODO's by priority 
 
 ### Battle
-* Rotation mechanics
-  * When moving, different kinds of speeds affect how you rotate. If you command a PC to move to a point it may or may not
-  need to rotate toward that point based on how fast it moves. 
-    * Walking: Can be rotated any direction while moving in any direction
-      * Things that involve walking are having shields up and sneaking
-    * Running: Can be rotated in any direction within a 180 degree arc centered on the direction of the run
-      * Examples: Running and gunning
-    * Sprint/Charging: Can be rotated in any direction within a 30-45 degree arc center on the direction of the sprint. 
-      * Example: Charging! Running like this burns stamina.
-  * TODO: Smooth rotation
-  * TODO: Different movement speeds
-  * TODO: Rotate based on movement speed rules
-* Shield arc-angle implemented
-  * Blue arc in the front of the character with variable range
-  * Zero stamina means the shield arc drops until some stamina recovers. 
 * Stamina represented (perhaps change the color of the sprite to red as it runs out, then blinking when they're out of stamina?)
   * A bar may be what we want in the future but it is probably more work to implement?
   * Stamina recovers at some rate. 
-* Add another player entity, implement selecting differnt entities
+* Add another player entity, implement selecting different entities
 * Make entities collide
 * Entities can attack each other on a cool-down for Melee attacks, costs stamina
 * Attacks are determined by clicking on another entity
 * Hitting an entity against the shield arc costs stamina. 
 * Health pips on character
 * Hitting against a side not covered by shield arc hits pips
+* Rotation mechanics
+  * When moving, different kinds of speeds affect how you rotate. If you command a PC to move to a point it may or may not
+  need to rotate toward that point based on how fast it moves. 
+    * Running: Can be rotated in any direction within a 180 degree arc centered on the direction of the run
+    Default movement speed.
+      * Examples: Running and gunning
+    * Sprint/Charging: Can be rotated in any direction within a 30-45 degree arc center on the direction of the sprint. 
+      * Example: Charging! Running like this burns stamina.
+  * TODO: Different movement speeds
+  * TODO: Rotate based on movement speed rules
+* Shield arc-angle implemented
+  * Zero stamina means the shield arc drops until some stamina recovers. 
 
 
 ### Overworld
--- core/src/com/ancient/game/crpg/RenderSystem.kt
+++ core/src/com/ancient/game/crpg/RenderSystem.kt
@@ -5,8 +5,10 @@ import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family.all
 import com.badlogic.ashley.systems.IteratingSystem
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.ashley.get
@@ -20,7 +22,7 @@ class RenderSystem(val batch: Batch, val viewport: Viewport) : IteratingSystem(
 
     private val renderMapper: ComponentMapper<Renderable> = mapperFor()
     private val transform: ComponentMapper<Transform> = mapperFor()
-
+    private val shapeRenderer = ShapeRenderer()
     private var toRender = mutableListOf<Entity>()
     override fun processEntity(entity: Entity, deltaTime: Float) {
         entity[renderMapper]?.let { toRender.add(entity) }
@@ -32,35 +34,90 @@ class RenderSystem(val batch: Batch, val viewport: Viewport) : IteratingSystem(
         toRender.clear()
     }
 
+    private class DrawData(
+            val x: FloatArray,
+            val y: FloatArray,
+            val r: FloatArray,
+            val sprites: Array<Sprite>)
+
     private fun draw(entities: List<Entity>) {
         viewport.camera.update()
-        batch.projectionMatrix = viewport.camera.combined
-        batch.begin()
-        entities.forEach { entity ->
-            val renderable = entity[renderMapper]!!
-            val rotation = entity[transform]!!.rotation
-            val sprite = renderable.sprite
-            val position = entity[transform]!!.position.let { viewport.project(it) }
-            val widthOffset = sprite.width / 2
-            val heightOffset = sprite.height / 2
+
+        DrawData(
+                FloatArray(entities.size),
+                FloatArray(entities.size),
+                FloatArray(entities.size),
+                entities.map {
+                    it[renderMapper]!!.sprite
+                }.toTypedArray()
+        ).apply {
+            entities.forEachIndexed { idx, entity ->
+                val position = entity[transform]!!.position.let { viewport.project(it) }
+                x[idx] = position.x
+                y[idx] = position.y
+                r[idx] = entity[transform]!!.rotation
+            }
+        }.let { data ->
 
 
-            batch.setColor(1f, 1f, 1f, 1f)
-            batch.draw(
-                    sprite,
-                    position.x - widthOffset,
-                    position.y - heightOffset,
-                    widthOffset,
-                    heightOffset,
-                    sprite.width,
-                    sprite.height,
-                    1f,
-                    1f,
-                   rotation
-            )
+            batch.projectionMatrix = viewport.camera.combined
+            batch.begin()
+            data.sprites.forEachIndexed { index, sprite ->
+                val widthOffset = sprite.width / 2
+                val heightOffset = sprite.height / 2
+
+
+                batch.setColor(1f, 1f, 1f, 1f)
+                batch.draw(
+                        sprite,
+                        data.x[index] - widthOffset,
+                        data.y[index] - heightOffset,
+                        widthOffset,
+                        heightOffset,
+                        sprite.width,
+                        sprite.height,
+                        1f,
+                        1f,
+                        data.r[index]
+                )
+
+            }
+            batch.end()
+
+            shapeRenderer.projectionMatrix = viewport.camera.combined
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
+            data.sprites.forEachIndexed { idx, sprite ->
+                shapeRenderer.apply {
+                    // Vulnerable
+                    color = Color.RED
+                    arc(
+                            data.x[idx],
+                            data.y[idx],
+                            sprite.width / 2,
+                            data.r[idx] + 90f,
+                            180f
+                    )
+
+                    // Defensive arc
+                    listOf(1f
+                            , 30f, 45f, 90f, 180f
+                    ).forEach { a ->
+                        color = Color(0f, 0f, (360f - a) / 360f, 1f)
+                        arc(
+                                data.x[idx],
+                                data.y[idx],
+                                sprite.width / 2,
+                                data.r[idx] - (a / 2),
+                                a
+                        )
+
+                    }
+                }
+            }
+            shapeRenderer.end()
 
         }
-        batch.end()
+
 
     }
 }
