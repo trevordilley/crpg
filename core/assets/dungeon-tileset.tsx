31181e109922f7814aaf49d14538c1b6a617bbbb
Trevor Dilley <trevor@twentyideas.com>
2019-04-13 07:08:00 -0700

    Added dungeon assets

--- core/assets/dungeon-tileset.tsx
+++ core/assets/dungeon-tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.3" name="dungeon-test" tilewidth="16" tileheight="16" tilecount="100" columns="10">
- <image source="../../../../Downloads/2D Pixel Dungeon Asset Pack/character and tileset/Dungeon_Tileset.png" width="160" height="160"/>
+ <image source="./Dungeon_Tileset.png" width="160" height="160"/>
 </tileset>
