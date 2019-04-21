35014280e5aa29b075637a25c984a9b0336e3bae
Trevor Dilley <trevor@twentyideas.com>
2019-04-21 06:47:40 -0700

    Added ui stuff for pausing and starting. Fancy UI is gonna suck...

--- core/src/main/kotlin/com/ancient/game/crpg/RenderSystem.kt
+++ core/src/main/kotlin/com/ancient/game/crpg/RenderSystem.kt
@@ -83,7 +83,6 @@ class RenderSystem(val batch: Batch, val viewport: Viewport,
             }
         }.let { data ->
 
-            val font = BitmapFont()
 
             batch.projectionMatrix = viewport.camera.combined
             batch.begin()
@@ -108,6 +107,35 @@ class RenderSystem(val batch: Batch, val viewport: Viewport,
                         data.r[index]
 
                 )
+            }
+            batch.end()
+
+
+            val originalMatrix = batch.projectionMatrix.cpy()
+            val uiMatrix = originalMatrix.scale(SiUnits.PIXELS_TO_METER, SiUnits.PIXELS_TO_METER, 1f)
+            batch.projectionMatrix = uiMatrix
+            batch.begin()
+            val font = BitmapFont()
+            if (UserInputManager.isPaused) {
+                font.color = Color.MAGENTA
+                font.draw(
+                        batch,
+                        "PAUSED",
+                        (viewport.worldWidth * SiUnits.UNIT) / 2f,
+                        200f
+                )
+            }
+            font.color = Color.WHITE
+
+
+            data.sprites.forEachIndexed { index, sprite ->
+                val width = sprite.width
+                val height = sprite.height
+
+
+                val widthOffset = width / 2
+                val heightOffset = height / 2
+                // https@//gamedev.stackexchange.com/questions/151624/libgdx-orthographic-camera-and-world-units
                 val healthLabel = data.healthData[index]?.let { hp -> "Health: $hp" }
                 val staminaLabel = data.staminaData[index]?.let { stm ->
                     val max = data.maxStaminaData[index]!!
@@ -115,13 +143,13 @@ class RenderSystem(val batch: Batch, val viewport: Viewport,
                 }
                 font.draw(
                         batch,
-//                        "$staminaLabel  $healthLabel",
-                        "",
-                        data.x[index] - widthOffset,
-                        data.y[index] + heightOffset + 10
+                        "$staminaLabel  $healthLabel",
+                        (data.x[index] * SiUnits.UNIT) - widthOffset,
+                        (data.y[index] * SiUnits.UNIT) + heightOffset
                 )
             }
             batch.end()
+            batch.projectionMatrix = originalMatrix
 
             shapeRenderer.projectionMatrix = viewport.camera.combined
             shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
